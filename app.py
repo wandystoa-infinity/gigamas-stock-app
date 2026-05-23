f6f76c42098edd77be1ee878d9e865f21b6b8945
wandystoa-infinity <stoainfinity42@gmail.com>
2026-05-24 01:19:53 +0700

    fix render port binding

--- app.py
+++ app.py
@@ -1878,4 +1878,5 @@ def not_found(error):
 # RUN
 # =========================================================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
